2c52a318514406af638743b8c85c308b3ce38d81
Ray Lawlor <ray@webcraftni.com>
2015-01-26 10:15:55 +0000

    Update enginer.php
    cleaning code

--- enginer.php
+++ enginer.php
@@ -11,51 +11,62 @@ defined('_JEXEC') or die('Restricted access');
 
 jimport('joomla.plugin.plugin');
 
-class plgSystemEnginer extends JPlugin {
+class plgSystemEnginer extends JPlugin
+{
 
-    public function onAfterInitialise() {
-		$engine = $this->params->get('engine');
-		$opposite = $this->_getOpposite($engine);
-		
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$query->select('TABLE_NAME');
-		$query->from('INFORMATION_SCHEMA.TABLES');
-		
-		$query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '"
-			. JFactory::getConfig()->get("db") 
-			. "' AND ENGINE = '" . $opposite . "'";
+    public function onAfterInitialise()
+    {
+        $engine = $this->params->get('engine');
+        $opposite = $this->_getOpposite($engine);
 
-		if($db->setQuery($query)){
-			$results = $db->loadObjectList();
-		} else {
-			return;
-		}
- 
-		if(!empty($results)) {
-			
-		foreach($results as $result) {
-			$tableName = $result->TABLE_NAME;
-			$newQuery = "ALTER TABLE " . $tableName . " ENGINE=" . $engine;
-			
-				try {
-					$db->setQuery($newQuery);
-					$db->execute();
-				}
-				catch (Exception $e){
-/* 					echo $e->getMessage(); */
-				}
-			}
-		}
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('TABLE_NAME');
+        $query->from('INFORMATION_SCHEMA.TABLES');
 
+        $query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '"
+                . JFactory::getConfig()->get("db")
+                . "' AND ENGINE = '" . $opposite . "'";
+
+        if ($db->setQuery($query))
+        {
+            $results = $db->loadObjectList();
+        }
+        else
+        {
+            return;
+        }
+
+        if (!empty($results))
+        {
+
+            foreach ($results as $result)
+            {
+                $tableName = $result->TABLE_NAME;
+                $newQuery = "ALTER TABLE " . $tableName . " ENGINE=" . $engine;
+
+                try
+                {
+                    $db->setQuery($newQuery);
+                    $db->execute();
+                } catch (Exception $e)
+                {
+                    /* echo $e->getMessage(); */
+                }
+            }
+        }
     }
-	
-	protected function _getOpposite($engine){
-		if($engine == 'MyISAM'){
-			return 'InnoDB';
-		}	else {
-			return 'MyISAM';
-		}
-	}
+
+    protected function _getOpposite($engine)
+    {
+        if ($engine == 'MyISAM')
+        {
+            return 'InnoDB';
+        }
+        else
+        {
+            return 'MyISAM';
+        }
+    }
 
 }
